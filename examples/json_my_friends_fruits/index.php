cd953c564a9b8f289e3f4d01daa64bbc2035be29
slavielle <sylvain.lavielle@netelios.fr>
2017-09-15 14:17:41 +0200

    Example

---
+++ examples/json_my_friends_fruits/index.php
@@ -0,0 +1,34 @@
+<?php
+
+$basePath = realpath(dirname(__FILE__));
+require_once($basePath . '/../../vendor/autoload.php');
+
+use Grabbag\Grabbag;
+
+echo "json input: \n";
+echo json_encode(json_decode(file_get_contents($basePath . '/data.json')), JSON_PRETTY_PRINT);
+echo "\n\n";
+echo "json output: \n";
+
+$json = json_encode(
+    Grabbag::grab(
+        json_decode(file_get_contents($basePath . '/data.json')),
+
+        // Here comes the Grabbag query
+        [
+            // Get all my friends list.
+            'my-friends:#any/name',
+
+            // Get all fruit they love.
+            'fruits-they-like:#any/food/liked/#any' => [
+                '.',
+
+                // If some of them love same fruit, no need to list it twice !
+                '?unique'
+            ]
+        ]
+    ),
+    JSON_PRETTY_PRINT
+);
+
+echo $json;
