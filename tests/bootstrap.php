bd52760b042823f1f599fac08f221414067b4778
slavielle <sylvain.lavielle@netelios.fr>
2017-09-21 21:35:57 +0200

    Improve autoload for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,13 @@
 <?php
 
+if(file_exists(__DIR__ . '/../vendor/autoload.php')){
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+if(file_exists(__DIR__ . '/../vendor/autoload.php')){
+    require_once __DIR__ . '/../../../../autoload.php';
+}
 // Testable classes
-require_once __DIR__ . '/../vendor/autoload.php';
+
 
 // Test useful classes
 require_once 'Grabbag/sourceData/Leaf1.php';
